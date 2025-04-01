1d358d0c2aee3afe45dc199eb95ebf47624ed06b
OL-surgeon <dasti888@ukr.net>
2025-04-01 16:32:42 +0300

    .

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   for (const number of numbers) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+//   return filteredNumbers;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+const styles = ["jazz", "blues"];
+styles.push("rock-n-roll");
+const index = styles.indexOf("blues");
+if (index !== -1) {
+  styles[index] = "classic";
+}
+function logItems(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(`${i + 1} - ${array[i]}`);
+  }
+}
+logItems(styles);
